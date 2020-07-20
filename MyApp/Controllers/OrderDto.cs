13b41a79f291ed37ec5827ba1a2826d85e4432df
Rahul Nath <rahulpnath@gmail.com>
2020-07-21 04:52:51 +1000

    Rearranged the order dto and model classes

---
+++ MyApp/Controllers/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿using MyApp.Models;
+
+namespace MyApp.Controllers
+{
+    public class OrderDto
+    {
+        public object ToOrder()
+        {
+            return new Order();
+        }
+    }
+}
